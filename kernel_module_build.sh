79b633d8c832531f847ea9a01b7a43a43f1d1ed9
Zachery Wu <wuzuoyu@huawei.com>
2021-09-27 14:31:16 +0800

    fix shell script
    Signed-off-by: Zachery Wu <wuzuoyu@huawei.com>

--- kernel_module_build.sh
+++ kernel_module_build.sh
@@ -17,42 +17,43 @@ set -e
 
 export OUT_DIR=$1
 export KERNEL_VERSION=$6
-if [ $2 == "small" ];then
+if [ "$2" == "small" ];then
     if [ ""x == $(which arm-linux-gnueabi-ld)x ] || [ ""x == $(which arm-linux-gnueabi-gcc)x ];then
         echo "Please install gcc-arm-linux-gnueabi, run \"sudo apt-get install gcc-arm-linux-gnueabi\"."
         exit 1
     fi
     export BUILD_TYPE=small
     LINUX_KERNEL_OUT=${OUT_DIR}/kernel/${KERNEL_VERSION}
-    if [ $3 != "" ];then
+    if [ "$3" != "" ];then
 	LLVM_PATH=$3
         export CLANG_CC=${LLVM_PATH}/bin/clang
     fi
-    if [ $5 == "hispark_taurus" ];then
+    if [ "$5" == "hispark_taurus" ];then
 	export DEVICE_NAME=hi3516dv300
     fi
-elif [ $2 == "standard" ];then
+elif [ "$2" == "standard" ];then
     export BUILD_TYPE=standard
     export DEVICE_NAME=$5
     LINUX_KERNEL_OUT=${OUT_DIR}/kernel/src_tmp/${KERNEL_VERSION}
     CLANG_BASE_PATH=$3
 fi
+LINUX_KERNEL_OBJ_OUT=${OUT_DIR}/kernel/OBJ/${KERNEL_VERSION}
 
 export OHOS_ROOT_PATH=$(pwd)/../../..
 export PRODUCT_PATH=$4
 
-LINUX_KERNEL_UIMAGE_FILE=${LINUX_KERNEL_OUT}/arch/arm/boot/uImage
+LINUX_KERNEL_UIMAGE_FILE=${LINUX_KERNEL_OBJ_OUT}/arch/arm/boot/uImage
 
 make -f kernel.mk
 
-if [ -f ${LINUX_KERNEL_UIMAGE_FILE} ];then
+if [ -f "${LINUX_KERNEL_UIMAGE_FILE}" ];then
     echo "uImage: ${LINUX_KERNEL_UIMAGE_FILE} build success"
 else
     echo "uImage: ${LINUX_KERNEL_UIMAGE_FILE} build failed!!!"
     exit 1
 fi
 
-if [ $2 == "small" ];then
+if [ "$2" == "small" ];then
     cp -rf ${LINUX_KERNEL_UIMAGE_FILE} ${OUT_DIR}/uImage_${DEVICE_NAME}_smp
 fi
 
