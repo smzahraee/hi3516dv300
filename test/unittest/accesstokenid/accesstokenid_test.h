0d462f97cf09b5eacaf476bb6c2f5a810a7ddf5d
wenfei <wenfei9@huawei.com>
2022-09-21 17:35:30 +0800

    Description:许可证修改 Apache==>GPL
    Signed-off-by: wenfei <wenfei9@huawei.com>

--- test/unittest/accesstokenid/accesstokenid_test.h
+++ test/unittest/accesstokenid/accesstokenid_test.h
@@ -1,10 +1,9 @@
 /*
  * Copyright (c) 2022 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: GPL-2.0
+#
+# Legacy blkg rwstat helpers enabled by CONFIG_BLK_CGROUP_RWSTAT.
+# Do not use in new code.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
