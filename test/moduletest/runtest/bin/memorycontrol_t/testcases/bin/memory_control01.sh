e29d9fabbefaf5dfc23af7acf0171d7192c525bb
wulisai <wulisai1711@126.com>
2022-11-19 09:26:48 +0800

    add mm/memcg_control.c testcase
    Signed-off-by: Ke Liu <liuke94@huawei.com>

---
+++ test/moduletest/runtest/bin/memorycontrol_t/testcases/bin/memory_control01.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+################################################################################
+#
+# Copyright (C) 2022 Huawei Device Co., Ltd.
+# SPDX-License-Identifier: GPL-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+# File: memory_control01.sh
+#
+# Description: Verify /dev/memcg/memory.app_score
+#
+# Authors:     Wu Lisai - wulisai@huawei-partners.com
+#
+# History:     Nov 18 2022 - init scripts
+#
+################################################################################
+source tst_oh.sh
+
+do_setup()
+{
+	zcat /proc/config.gz | grep CONFIG_HYPERHOLD_MEMCG=y || tst_res TCONF "CONFIG_HYPERHOLD_MEMCG=y not satisfied!"
+}
+
+do_test()
+{
+       tst_res TINFO "Start to verify mem ."
+
+        mem=$(cat /dev/memcg/memory.app_score)
+        echo $mem
+        if [ "$mem"x = "300"x ]; then
+               tst_res TPASS "memory.app_score value correct."
+        else
+               tst_res TFAIL "memory.app_score value incorrect."
+        fi
+}
+
+do_clean()
+{
+
+}
+
+do_setup
+do_test
+do_clean
+tst_exit
